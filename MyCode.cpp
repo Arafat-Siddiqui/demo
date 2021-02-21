404f6218b04d78d39608bb47745f8a185f6072d9
Arafat <arafat_siddiqui@persistent.co.in>
2021-02-21 22:51:25 +0530

    initial Commit

---
+++ MyCode.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	for(int i=1;i<10;i++)
+		cout<<i;
+
+	for(int i=1;i<10;i++)
+		cout<<i*i;
+
+	for(int i=1;i<10;i++)
+		cout<<i*i*i;
+
+	return 0;
+}
